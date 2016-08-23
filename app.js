bca6fd98022889e633088646047e539688eed363
Mattressburn <chevyboxer@gmail.com>
2016-08-23 14:38:59 -0400

    add buttons

--- app.js
+++ app.js
@@ -4,19 +4,29 @@
 // click function append 
 // capture value of text input
 
-$(document).ready(function(){
+$(document).ready(function() {
+	
+
 	$('#js-shopping-list-form').submit(function(event){
-	$('.shopping-list').append(
-		'<li>'+ 
-	 '<span class="shopping-item">'+$('#shopping-list-entry').val() +'</span>'
-	 +'<div class="shopping-item-controls">' + '<button class="shopping-item-toggle">' 
-	 +' <span class="button-label">delete</span>' +'<span class="button-label">check</span>'
-	);	
-$()
-$('.shopping-list').on("click","shopping-item-delete", function(event){
-	$(this).closest("li").remove();
+		$('.shopping-list').append(
+			'<li>'+ 
+				'<span class="shopping-item">'+$('#shopping-list-entry').val() +'</span>'
+				+'<div class="shopping-item-controls">' + '<button class="shopping-item-toggle">' 
+				+' <span class="button-label">delete</span></button>' +'<button><span class="button-label">check</span></button>'
+				+'</div></li>'
+		);
+		$('#shopping-list-entry').val('');
+		event.preventDefault();
+	});	
+
+
+		$('.shopping-list').on("click",".shopping-item-delete", function(event){
+			$(this).closest("li").remove();
+		});
+
+		$('.shopping-list').on("click",".shopping-item-toggle", function(event){
+			$(this).closest("li").find("shopping-item").toggleClass(".shopping-item_checked");
+		});
+
 
 });
-
-  $('li').click(function(event) {
-    this.remove();
