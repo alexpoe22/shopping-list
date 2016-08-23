1f7899df25223ec7e34f4dd2a3df36d842875c2c
Mattressburn <chevyboxer@gmail.com>
2016-08-23 15:25:16 -0400

    fix button for adding groceries

--- app.js
+++ app.js
@@ -10,22 +10,29 @@ $(document).ready(function() {
 	$('#js-shopping-list-form').submit(function(event){
 		$('.shopping-list').append(
 			'<li>'+ 
-				'<span class="shopping-item">'+$('#shopping-list-entry').val() +'</span>'
-				+'<div class="shopping-item-controls">' + '<button class="shopping-item-toggle">' 
-				+' <span class="button-label">delete</span></button>' +'<button><span class="button-label">check</span></button>'
-				+'</div></li>'
+				'<span class="shopping-item">'+ $('#shopping-list-entry').val() +'</span>' +
+				'<div class="shopping-item-controls">' + 
+					'<button class="shopping-item-toggle">' + ' <span class="button-label">check</span></button>' +
+					
+					'<button class="shopping-item-delete">' + ' <span class="button-label">delete</span></button>'+
+				'</div>'+
+				'</li>'
 		);
 		$('#shopping-list-entry').val('');
 		event.preventDefault();
 	});	
 
 
-		$('.shopping-list').on("click",".shopping-item-delete", function(event){
+		$(".shopping-list").on("click",".shopping-item-delete", function(event){
+			
+
 			$(this).closest("li").remove();
 		});
 
-		$('.shopping-list').on("click",".shopping-item-toggle", function(event){
-			$(this).closest("li").find("shopping-item").toggleClass(".shopping-item_checked");
+		$(".shopping-list").on("click",".shopping-item-toggle", function(event){
+			
+
+			$(this).closest("li").find(".shopping-item").CSS(".shopping-item_checked");
 		});
 
 
